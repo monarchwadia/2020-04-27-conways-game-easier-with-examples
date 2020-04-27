4588191c62c627f5350d732edc2a102102b0fe6e
Monarch Wadia <monarch@zeroprojects.ca>
2020-04-27 13:13:02 -0400

    Build

---
+++ docs/precache-manifest.85a364862e93ee5427b8a1f15f393813.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09d508a875daa814c1df17cb15ae682c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19589088e9d1574d69f0",
+    "url": "/static/css/main.4941300d.chunk.css"
+  },
+  {
+    "revision": "13e315246d8160423c27",
+    "url": "/static/js/2.052d43f8.chunk.js"
+  },
+  {
+    "revision": "299d8b1ee45aa90f5be74e25b06a2efd",
+    "url": "/static/js/2.052d43f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19589088e9d1574d69f0",
+    "url": "/static/js/main.3c27cb7d.chunk.js"
+  },
+  {
+    "revision": "d1bac09240914957ce7f",
+    "url": "/static/js/runtime-main.b610cad6.js"
+  }
+]);
