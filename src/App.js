efe964e5de374adad4723af92e29106a0446dc38
Monarch Wadia <monarch@zeroprojects.ca>
2020-04-27 13:00:36 -0400

    Added a basic version of the game. Moved challenge elements to their own file.

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { ConwaysGameEngine } from '@monarchwadia/conways-game-engine';
+import './App.css';
+
+class App extends Component {
+  state = {
+    engine: null,
+    interval: null
+  }
+
+  componentDidMount() {
+    const engine = new ConwaysGameEngine();
+  
+    engine.draw(3, 4);
+    engine.draw(4, 5);
+    engine.draw(5, 5);
+    engine.draw(5, 4);
+    engine.draw(5, 3);
+
+    const interval = setInterval(() => {
+      engine.step();
+      this.forceUpdate();
+    }, 1000)
+
+    this.setState({ engine, interval });
+  }
+
+  render() {    
+    if (!this.state.engine) {
+      return null;
+    }
+
+    const world = this.state.engine.world.map((row, rowI) => (
+      <div className="row" key={rowI}>
+        {
+          row.map((cell, cellI) => (
+            <span className="cell" key={`${rowI}-${cellI}-${cell}`}> { cell ? "#" : "." } </span>
+          ))
+        }
+      </div>
+    ))
+  
+    return (
+      <div className="App">
+        { world }
+      </div>
+    );
+  }
+}
+
+export default App;
